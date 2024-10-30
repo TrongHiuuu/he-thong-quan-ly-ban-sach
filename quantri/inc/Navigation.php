6a65ad8c130ef28cab40a46361b456881fb09425
Quỳnh Hương <quynhhuong.nguyenpham2004@gmail.com>
2024-10-31 00:50:36 +0700

    Update 31/10: Cập nhật link cho sidebar
    Khi nhấn vào mục trong side bar thì di chuyển đến trang tương ứng

--- quantri/inc/Navigation.php
+++ quantri/inc/Navigation.php
@@ -25,55 +25,55 @@
             <div class="offcanvas-body">
                 <ul class="nav flex-column">
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=role" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-crown me-3"></i>
                             Quản lý nhóm quyền
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=account" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-user me-3"></i>
                             Quản lý tài khoản
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=author" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-at me-3"></i>
                             Quản lý tác giả
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=category" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-list me-3"></i>
                             Quản lý danh mục
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=supplier" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-address-book me-3"></i>
                             Quản lý nhà cung cấp
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=discount" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-badge-percent me-3"></i>
                             Quản lý mã giảm giá
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=product" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-books me-3"></i>
                             Quản lý sản phẩm
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=order" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-boxes-stacked me-3"></i>
                             Quản lý đơn hàng
                         </a>
                     </li>
                     <li class="nav-item sidebar-item">
-                        <a href="#" class="nav-link text-black fs-5 align-items-center">
+                        <a href="?page=grn" class="nav-link text-black fs-5 align-items-center">
                             <i class="fa-regular fa-boxes-packing me-3"></i>
                             Quản lý phiếu nhập
                         </a>
@@ -91,19 +91,19 @@
                         </a>
                         <ul class="dropdown-menu border-0 w-100">
                             <li class="dropdown-item">
-                                <a href="#" class="nav-link text-black fs-5 align-items-center">
+                                <a href="?page=income" class="nav-link text-black fs-5 align-items-center">
                                     <i class="fa-regular fa-chart-pie-simple me-3"></i>
                                     Thống kê doanh thu
                                 </a>
                             </li>
                             <li class="dropdown-item">
-                                <a href="#" class="nav-link text-black fs-5 align-items-center">
+                                <a href="?page=cost" class="nav-link text-black fs-5 align-items-center">
                                     <i class="fa-regular fa-file-chart-pie me-3"></i>
                                     Thống kê nhập kho
                                 </a>
                             </li>
                             <li class="dropdown-item">
-                                <a href="#" class="nav-link text-black fs-5 align-items-center">
+                                <a href="?page=profit" class="nav-link text-black fs-5 align-items-center">
                                     <i class="fa-regular fa-chart-pie-simple-circle-dollar me-3"></i>
                                     Thống kê lợi nhuận
                                 </a>
@@ -118,15 +118,6 @@
         <div class="nav justify-content-end d-flex align-items-center">
             <div class="nav-item me-3">
                 <a href="#" class="nav-link text-dark">
-                    <!-- <i  class="fa-solid fa-circle-user" 
-                        style="color: #087b44; font-size: 40px;"
-                    >
-                    </i>
-                    <span class="badge rounded-pill position-absolute text-white fw-semibold start-50 bottom-0 translate-middle-x" 
-                        style="max-width: 80px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; background-color: #087b44;"
-                    >
-                        Hương
-                    </span> -->
                     <span class="account-name">
                         <i class="fa-regular fa-user-crown me-2 fs-4"></i>
                         HuongLamCoder
